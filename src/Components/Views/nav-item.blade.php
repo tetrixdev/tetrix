550305abc911adf81e2656c20596fb8dfc7bb286
Jasper Bauer <jasper@tetrix.dev>
2025-04-01 22:48:10 +0200

    Updating code formatting slightly

--- src/Components/Views/nav-item.blade.php
+++ src/Components/Views/nav-item.blade.php
@@ -1,52 +1,52 @@
 <?PHP
 
-$dom = new DOMDocument();
+if(isset($dropdown)) {
+    $dom = new DOMDocument();
 
-// Suppress warnings about invalid HTML tags etc.
-@$dom->loadHTML("<body>$dropdown</body>");
+    // Suppress warnings about invalid HTML tags etc.
+    @$dom->loadHTML("<body>$dropdown</body>");
 
-$body = $dom->getElementsByTagName('body')->item(0);
+    $body = $dom->getElementsByTagName('body')->item(0);
 
-$columnCount = 0;
-if ($body !== null) {
-    foreach ($body->childNodes as $node) {
-        // Only count if this is a top-level element node
-        if ($node->nodeType === XML_ELEMENT_NODE) {
-            $columnCount++;
+    $columnCount = 0;
+    if ($body !== null) {
+        foreach ($body->childNodes as $node) {
+            // Only count if this is a top-level element node
+            if ($node->nodeType === XML_ELEMENT_NODE) {
+                $columnCount++;
+            }
         }
     }
-}
 
-// This might look odd, but otherwise vite will not compile the class correctly (classes need to be mentioned in full)
-$columnClass = '';
-switch($columnCount) {
-    case 1:
-        $columnClass = 'grid-cols-1';
-        break;
-    case 2:
-        $columnClass = 'grid-cols-2';
-        break;
-    case 3:
-        $columnClass = 'grid-cols-3';
-        break;
-    case 4:
-        $columnClass = 'grid-cols-4';
-        break;
-    case 5:
-        $columnClass = 'grid-cols-5';
-        break;
-    case 6:
-        $columnClass = 'grid-cols-6';
-        break;
-    case 7:
-        $columnClass = 'grid-cols-7';
-        break;
-    case 8:
-        $columnClass = 'grid-cols-8';
-        break;
-}
+    // This might look odd, but otherwise vite will not compile the class correctly (classes need to be mentioned in full)
+    $columnClass = '';
+    switch($columnCount) {
+        case 1:
+            $columnClass = 'grid-cols-1';
+            break;
+        case 2:
+            $columnClass = 'grid-cols-2';
+            break;
+        case 3:
+            $columnClass = 'grid-cols-3';
+            break;
+        case 4:
+            $columnClass = 'grid-cols-4';
+            break;
+        case 5:
+            $columnClass = 'grid-cols-5';
+            break;
+        case 6:
+            $columnClass = 'grid-cols-6';
+            break;
+        case 7:
+            $columnClass = 'grid-cols-7';
+            break;
+        case 8:
+            $columnClass = 'grid-cols-8';
+            break;
+    }
 
-if(isset($dropdown)) {
     $rawDropdown = $dropdown;
 
     // Remove outer x-tx::sub-nav-column
@@ -60,13 +60,13 @@ if(isset($dropdown)) {
 ?>
 
 <div @isset($dropdown)
-         x-data="{ open: false }"
+     x-data="{ open: false }"
      @else
-         @click="navOpen = false"
+     @click="navOpen = false"
      @endisset
      class="relative w-full {{ config('tetrix.nav.breakpoint') }}:w-auto">
     <div @isset($dropdown)
-             @click="open = !open"
+         @click="open = !open"
          x-ref="navitem"
          :class="{'bg-tx-general-150 dark:bg-tx-general-800': open }"
          @endisset
@@ -80,47 +80,45 @@ if(isset($dropdown)) {
     @isset($dropdown)
         {{-- Desktop --}}
         <div class="hidden {{ config('tetrix.nav.breakpoint') }}:block">
-            <div
-                    x-show="open"
-                    x-cloak
-                    x-anchor.placement.bottom-start.offset.8="$refs.navitem"
-                    @click.outside="open = false"
-                    x-transition:enter="transition ease-out duration-100"
-                    x-transition:enter-start="opacity-0 scale-95"
-                    x-transition:enter-end="opacity-100 scale-100"
-                    x-transition:leave="transition ease-in duration-75"
-                    x-transition:leave-start="opacity-100 scale-100"
-                    x-transition:leave-end="opacity-0 scale-95"
-                    class="absolute
-                   w-max
-                   bg-tx-general-0 dark:bg-tx-general-900 border
-                   rounded border-tx-general-300 dark:border-tx-general-800
-                   shadow-md
-                   z-10
-                   grid {{ $columnClass }}">
+            <div x-show="open"
+                 x-cloak
+                 x-anchor.placement.bottom-start.offset.8="$refs.navitem"
+                 @click.outside="open = false"
+                 x-transition:enter="transition ease-out duration-100"
+                 x-transition:enter-start="opacity-0 scale-95"
+                 x-transition:enter-end="opacity-100 scale-100"
+                 x-transition:leave="transition ease-in duration-75"
+                 x-transition:leave-start="opacity-100 scale-100"
+                 x-transition:leave-end="opacity-0 scale-95"
+                 class="absolute
+                        w-max
+                        bg-tx-general-0 dark:bg-tx-general-900
+                        border rounded border-tx-general-300 dark:border-tx-general-800
+                        shadow-md
+                        z-10
+                        grid {{ $columnClass }}">
                 {{ $dropdown }}
             </div>
         </div>
         {{-- Mobile --}}
         <div class="block {{ config('tetrix.nav.breakpoint') }}:hidden">
-            <div
-                    x-show="open"
-                    x-cloak
-                    @click.outside="open = false"
-                    x-transition:enter="transition ease-out duration-100"
-                    x-transition:enter-start="opacity-0 scale-95"
-                    x-transition:enter-end="opacity-100 scale-100"
-                    x-transition:leave="transition ease-in duration-75"
-                    x-transition:leave-start="opacity-100 scale-100"
-                    x-transition:leave-end="opacity-0 scale-95"
-                    class="absolute
-                       left-0 top-full m-2
-                       w-[calc(100%-16px)]
-                       bg-tx-general-0 dark:bg-tx-general-900
-                       border rounded border-tx-general-300 dark:border-tx-general-800
-                       shadow-md
-                       z-10
-                       grid grid-cols-1">
+            <div x-show="open"
+                 x-cloak
+                 @click.outside="open = false"
+                 x-transition:enter="transition ease-out duration-100"
+                 x-transition:enter-start="opacity-0 scale-95"
+                 x-transition:enter-end="opacity-100 scale-100"
+                 x-transition:leave="transition ease-in duration-75"
+                 x-transition:leave-start="opacity-100 scale-100"
+                 x-transition:leave-end="opacity-0 scale-95"
+                 class="absolute
+                        left-0 top-full m-2
+                        w-[calc(100%-16px)]
+                        bg-tx-general-0 dark:bg-tx-general-900
+                        border rounded border-tx-general-300 dark:border-tx-general-800
+                        shadow-md
+                        z-10
+                        grid grid-cols-1">
                 {!! $flattenedHtml !!}
             </div>
         </div>
